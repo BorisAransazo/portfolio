d04057a00d32a86585eae790783ad1a51963a6f9
BorisAransazo <109746036+BorisAransazo@users.noreply.github.com>
2022-07-21 22:26:15 +0800

    Add files via upload

---
+++ portfolio/index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Boris Aransazo</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+
+ 
+
+</head>
+<body data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="50">
+
+<div class="main" id="Home">
+ 
+     
+  	
+  	<img src="image/indexpic.png" class="container_image"> 
+ <div class="boris">
+ 	<h3 class="p">Hi! I'm a Back-end Developer</h3>
+ </div> 	
+	
+	 
+		
+	
+<br>
+</div>
+
+
+
+   
+
+
+
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  <div class="container-fluid">
+  
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+      	<li class="nav-item">
+        <a class="nav-link" href="#Home">Home</a>
+      </li>
+    	<li class="nav-item">
+        <a class="nav-link" href="#Education">Education</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#Skills">Skills</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#About">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#Contact">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#Link">Link</a>
+      </li>
+    </ul>
+    </div>
+  </div>
+</nav>
+</div>
+
+
+
+
+<div class="container mt-5">
+	  	    <div class="col-sm-4">
+	  
+				<div id="Education" class="" style="padding:100px 10px; ">
+				  <h1>Education</h1>
+				  <h3>College</h3>	
+					      	<h4>Bulacan Polytechnic College</h4>
+					      		<h6>(Bulihan, City of Malolos, Bulacan)</h6>	
+					      	  <h5>Associate in Computer Technology</h5>
+					      	   <p>2018 - 2020</p>
+					      	   <h4>Bulacan Polytechnic College</h4>
+					      	   <h6>(Bulihan, City of Malolos, Bulacan)</h6>		
+					      	  <h5>Bachelor of Science in Information Systems</h5>
+					      	   <p>2020 - 2022</p>
+					      	 
+				</div>
+
+			</div>
+  		<hr class="h">
+
+
+</div>
+
+<div class="container mt-5">
+			
+			    <div class="col-sm-4">
+
+						<div id="Skills" class="" style="padding:100px 10px; ">
+						  <h1>Skills</h1>
+						 
+						  
+						  <h6>HTML</h6><div class="progress">
+						    <div class="progress-bar" style="width:50%">50%</div>
+						  </div>
+
+
+						  <h6>CSS</h6><div class="progress">
+						    <div class="progress-bar" style="width:30%">30%</div>
+						  </div>
+
+						  <h6>PHP</h6><div class="progress">
+						    <div class="progress-bar" style="width:40%">40%</div>
+						  </div>
+
+						  <h6>JAVASCRIPT</h6><div class="progress">
+						    <div class="progress-bar" style="width:40%">40%</div>
+						  </div>
+
+						  <h6>PYTHON</h6><div class="progress">
+						    <div class="progress-bar" style="width:40%">40%</div>
+						  </div>
+
+					</div>
+			  
+			</div>
+			<hr class="h">
+</div>
+
+<div class="container mt-5">
+		<div class="col-sm-4">
+			<div id="About" class="" style="padding:100px 10px; ">
+		  <h1>About</h1>
+		  <p>LOREM IPSUM</p>
+			</div>
+	</div>
+<hr class="h">
+</div>
+
+<div class="container mt-5">
+		<div class="col-sm-4">
+			<div id="Contact" class="" style="padding:100px 10px;">
+			  <h1>Contact</h1>
+				<h6><i class="fa fa-phone" aria-hidden="true" ></i>+639488183371</h6>
+				<h6><i class="fa fa-envelope-o" aria-hidden="true"></i> boris.aransazo25@gmail.com</h6>
+		 	</div>
+		</div>
+<hr class="h">
+
+</div>
+
+<div class="container mt-5">
+		<div class="col-sm-4">
+			<div id="Link" class="" style="padding:100px 20px;">
+			  <h1>Link</h1>
+			   <a href="https://www.facebook.com/boris.sandiko.aransazo/" class="fa fa-facebook"></a>
+			   <a href="https://www.linkedin.com/in/boris-aransazo-b702a619b/" class="fa fa-linkedin"></a>
+			</div>
+		</div>
+</div>
+
+
+
+
+	
+   
+</body>
+</html>
+
+
+
+
